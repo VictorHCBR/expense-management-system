bd1684d68c2de22a64d60aee4df0b2bc242d0b70
Victor Campos <victortheveloper@gmail.com>
2026-01-31 21:47:14 -0300

    feat: Adição da definição da entidade 'Transaction'.

--- core/Manager/Manager.Domain/Entities/Transaction.cs
+++ core/Manager/Manager.Domain/Entities/Transaction.cs
@@ -1,9 +1,34 @@
-﻿namespace Manager.Domain.Entities;
+﻿using Manager.Domain.Common;
+using Manager.Domain.Enums;
+
+namespace Manager.Domain.Entities;
 
 /// <summary>
-/// 
+/// Transação financeira.
+/// Regras de domínio principais são validadas no Application Service (caso de uso),
+/// pois dependem de consulta de Pessoa/Categoria (ex.: idade, finalidade).
+/// Aqui valido somente invariantes "locais", que são descrição e valor positivo.
 /// </summary>
 public sealed class Transaction
 {
+    public Guid Id { get; private set; } = Guid.CreateVersion7();
+    public string Description { get; private set; } = string.Empty;
+    public decimal Amount { get; private set; }
+    public TransactionType Type { get; private set; }
 
+    public Guid PersonId { get; private set; }
+    public Guid CategoryId { get; private set; }
+
+    //EF Core
+    private Transaction() { }
+
+    public Transaction(string description, decimal amount, TransactionType type, Guid personId, Guid categoryId)
+    {
+        ValidationHelper.SetDescription(description);
+        ValidationHelper.SetAmount(amount);
+
+        Type = type;
+        PersonId = personId;
+        CategoryId = categoryId;
+    }
 }
